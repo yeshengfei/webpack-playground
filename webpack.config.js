02f0abb49cdeb1e4c7d95a822ee408099f8a14e0
Moon <191705595@qq.com>
2017-04-05 16:25:36 +0800

    忽略对于node_modules的关注，以降低对于cpu的消耗

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+var webpack = require("webpack");
 var path = require("path")
+
 
 module.exports = {
     entry: {
@@ -7,8 +9,21 @@ module.exports = {
     devServer: {
         inline:true,
         contentBase: './dist',
-        port:8083
+        port:8083,
+        watchOptions: {
+            // Delay the rebuild after the first change
+            aggregateTimeout: 300,
+            // Poll using interval (in ms, accepts boolean too)
+            poll: 1000,
+        },
     },
+    plugins: [
+        // Ignore node_modules so CPU usage with poll
+        // watching drops significantly.
+        new webpack.WatchIgnorePlugin([
+            path.join(__dirname, 'node_modules')
+        ]),
+    ],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
