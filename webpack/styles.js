15c454ba54dabb907d50635a75c082df5124b10a
Moon <191705595@qq.com>
2017-04-16 04:21:01 +0800

    引入postcss-loader做浏览器词缀自动适配

--- webpack/styles.js
+++ webpack/styles.js
@@ -1,12 +1,21 @@
+const postCssLoader = {
+  loader: 'postcss-loader',
+  options: {
+    plugins: () => ([
+        require('autoprefixer')    
+    ]),
+  },
+}
+
 module.exports = {
     rules : [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader',postCssLoader],
       },
       {
         test: /\.less$/,
-        use: ['style-loader', 'css-loader', 'less-loader'],
+        use: ['style-loader', 'css-loader', 'less-loader',postCssLoader],
       },
     ]
 }
